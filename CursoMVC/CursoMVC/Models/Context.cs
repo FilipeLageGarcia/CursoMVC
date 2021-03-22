a6fffac8441e3d77eb633a8cff85e61a427be469
FilipeLageGarcia <filipelagegarcia@gmail.com>
2021-03-22 09:53:52 -0300

    Concluido

---
+++ CursoMVC/CursoMVC/Models/Context.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CursoMVC.Models;
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Categoria> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Filipe\Documents\Cursomvc.mdf;Integrated Security = True");
+        }
+
+        public DbSet<CursoMVC.Models.Produto> Produto { get; set; }
+    }
+}
